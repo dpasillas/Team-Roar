f6e6ccb026f0afdde0891ddbc4d3064b55d67aad
Darrin Lin <lindarrin@gmail.com>
2014-06-10 07:57:21 -0700

    Skybox and Victory/Gameovers creens

---
+++ nexis/Assets/Scripts/GameOverScripts/ContinueButton.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var continue_button_idle : Texture2D;
+var continue_button_hover : Texture2D;
+
+function Start () {
+
+}
+
+function Update () {
+	
+}
+
+function OnMouseEnter()
+{
+	guiTexture.texture = continue_button_hover;
+}
+
+function OnMouseExit()
+{
+	guiTexture.texture = continue_button_idle;
+}
+
+function OnMouseDown()
+{
+	Application.LoadLevel("StatChangeMenu");
+}
---
+++ nexis/Assets/Scripts/GameOverScripts/RetryButton.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var retry_button_idle : Texture2D;
+var retry_button_hover : Texture2D;
+
+function Start () {
+
+}
+
+function Update () {
+	
+}
+
+function OnMouseEnter()
+{
+	guiTexture.texture = retry_button_hover;
+}
+
+function OnMouseExit()
+{
+	guiTexture.texture = retry_button_idle;
+}
+
+function OnMouseDown()
+{
+	Application.LoadLevel("StatChangeMenu");
+}
